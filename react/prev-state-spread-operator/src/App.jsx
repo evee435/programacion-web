7671f2cdddd9ef22fda8c30b3d884ebc725b734e
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-09-03 17:08:24 -0300

    realice la ultima actividad para sumar los 10 puntos

--- react/prev-state-spread-operator/src/App.jsx
+++ react/prev-state-spread-operator/src/App.jsx
@@ -5,20 +5,32 @@ function App() {
   const[num,setNum] = useState([1,2,3,4])
   const[persona, setPersona] = useState ({nombre: "evelyn", edad: 17})
  
+    //hacer que el setNum agregue numeros consecutivos -> 3 puntos
+
   function myFunction(){
     const ultimo = num[num.length - 1] //obtiene el ultimo numero del array
     setNum([...num,ultimo +1 ])//genera el siguiente consecutivo y se agrega en la lista
 
   }
-  function Persona() {
+
+  //Agregar una propiedad al objeto persona usando spread operator y que al hacer click en un boton dispare la funcion y la muestre por consola -> 4 puntos
+
+  function nuevaPersona() {
     const nuevaPersona = {...persona, apellido: "Villarreal" // se agrega una nueva propiedad
     }
     setPersona(nuevaPersona)
     console.log(nuevaPersona) // se muestra en consola
   }
 
-  //tarea hacer que el setNum agregue numeros consecutivos -> 3 puntos
-  //Agregar una propiedad al objeto persona usando spread operator y que al hacer click en un boton dispare la funcion y la muestre por consola -> 4 puntos
+  //cambiar el nombre del objeto original
+    function cambiarPersona() {
+    const nuevaPersona = {...persona, nombre: "clara" // se agrega una nueva propiedad
+    }
+    setPersona(nuevaPersona)
+    console.log(nuevaPersona) // se muestra en consola
+  }
+
+
   return (
     <>
      <div>
@@ -27,7 +39,9 @@ function App() {
 
       ))}
       <button onClick={myFunction}>agregar numero</button>
-      <button onClick={Persona}>agrega propiedad</button>
+      <button onClick={nuevaPersona}>agrega propiedad</button>
+      <button onClick={cambiarPersona}>cambia el nombre</button>
+      <p>Nombre {persona.nombre}</p>
      </div>
     </>
   )
